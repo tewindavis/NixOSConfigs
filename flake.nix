cde7e1bde9ae90ac7d24b02c00260576c8217398
td <tewindavis@gmail.com>
2026-05-17 03:26:16 +0000

    yes

--- flake.nix
+++ flake.nix
@@ -6,14 +6,23 @@
   };
 
   outputs = { self, nixpkgs, hyprland, ...}@inputs: {
-    nixosConfigurations.utm-vm = nixpkgs.lib.nixosSystem {
-      # May we never be forced to use x86 again...
-      system = "aarch64-linux";
-      specialArgs = {inherit inputs;};
-      modules = [
-        ./hosts/utm-vm/configuration.nix
-        hyprland.nixosModules.default
-      ];
+    nixosConfigurations = {
+     utm-vm = nixpkgs.lib.nixosSystem {
+       # May we never be forced to use x86 again...
+       system = "aarch64-linux";
+       specialArgs = {inherit inputs;};
+       modules = [
+         ./hosts/utm-vm/configuration.nix
+         hyprland.nixosModules.default
+       ];
+     };
+     framework = nixpkgs.lib.nixosSystem {
+       system = "x86_64-linux";
+       specialArgs = {inherit inputs;};
+       modules = [
+         ./hosts/framework/configuration.nix
+         hyprland.nixosModules.default
+       ];
     };
   };
 }
